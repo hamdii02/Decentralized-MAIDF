86e25088dc38ef1cfd48b175991ba29724fe14d1
Hamdi <46610259+hamdii02@users.noreply.github.com>
2023-03-23 12:55:03 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get install python3
 apt install python3-pip
-pip3 install iperf3
-pip3 install elasticsearch elasticsearch-async pytz
+pip3 install --yes iperf3
+pip3 install --yes elasticsearch elasticsearch-async pytz
